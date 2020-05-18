c441ce4c89b42f445f09983527cd1c8c5a479014
Joel Davis <joeladavisdev@gmail.com>
2020-05-18 16:06:11 -0600

    How to Install Axios in React to Communicate with APIs

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import moment from "moment";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -7,9 +8,29 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match.js";
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.getPortfolioItems = this.getPortfolioItems.bind(this)
+  }
+  getPortfolioItems() {
+// Make a request for a user with a given ID
+axios.get("https://joeldavis.devcamp.space/portfolio/portfolio_items")
+  .then((response) => {
+    // handle success
+    console.log(response);
+  })
+  .catch((error) => {
+    // handle error
+    console.log(error);
+  })
+ 
+  }
   render() {
+    this.getPortfolioItems()
     return (
       <div className="app">
         <Router>
@@ -23,6 +44,9 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+
             </Switch>
           </div>
         </Router>

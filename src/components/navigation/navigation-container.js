b244d0f8f43c175a088143d84edced8d15f5861c
Joel Davis <joeladavisdev@gmail.com>
2020-05-19 16:46:03 -0600

    Integrating scss variables

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -10,30 +10,32 @@ export default class NavigationComponent extends Component {
     return (
       <div className="nav-wrapper">
         <div className="left-side">
-          <div className="nav-link-wrapper">          
-        <NavLink exact to="/" activeClassName="nav-link-active">
-          Home
-        </NavLink>
-        </div>
-        <div className="nav-link-wrapper">
-        <NavLink to="/about-me" activeClassName="nav-link-active">
-          About
-        </NavLink>
-        </div>
-        <div className="nav-link-wrapper">
-        <NavLink to="/contact" activeClassName="nav-link-active">
-          Contact
-        </NavLink>
-        </div>
-        <div className="nav-link-wrapper">
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
-        </NavLink>
+          <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="nav-link-active">
+              Home
+            </NavLink>
+          </div>
+
+          <div className="nav-link-wrapper">
+            <NavLink to="/about-me" activeClassName="nav-link-active">
+              About
+            </NavLink>
+          </div>
+
+          <div className="nav-link-wrapper">
+            <NavLink to="/contact" activeClassName="nav-link-active">
+              Contact
+            </NavLink>
+          </div>
+
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">
+              Blog
+            </NavLink>
+          </div>
         </div>
 
-        {false ? <button>Add Blog</button> : null}
-        </div>
-        <div className="right-side">JOEL DAVIS</div>
+        <div className="right-side">JORDAN HUDGENS</div>
       </div>
     );
   }

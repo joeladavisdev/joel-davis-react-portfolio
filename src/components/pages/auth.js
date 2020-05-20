fd93deb9f27db344c4191de9df2b95db6bc6fc19
Joel Davis <joeladavisdev@gmail.com>
2020-05-20 15:56:39 -0600

    How to import static images

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
+import loginImg from "../../../static/assets/images/login.jpg"
 
 export default class Auth extends Component {
   render() {
-    return <div>Auth....</div>;
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`
+          }}
+        />
+
+        <div className="rightColumn">
+          <h1>Login compo</h1>
+        </div>
+      </div>
+    )
   }
 }

95d0b924e2be504b898a021b6bf520d752114e8c
Joel Davis <joeladavisdev@gmail.com>
2020-05-20 11:55:27 -0600

    Building structure...

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -74,8 +74,10 @@ export default class PortfolioContainer extends Component {
         <button onClick={() => this.handleFilter("Enterprise")}>
           Enterprise
         </button>
-
-        {this.portfolioItems()}
+        <div className="portfolio-items-wrapper">
+          {this.portfolioItems()}
+        </div>
+        
       </div>
     );
   }

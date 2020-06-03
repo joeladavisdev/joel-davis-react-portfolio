eeca74d1cd468d6687962d2db470487995ee05eb
Joel <joeladavisdev@gmail.com>
2020-06-02 18:05:56 -0600

    Building About Page Styles for React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,29 @@
 import React from "react";
+import profilePicture from "../../../static/assets/images/bio/JoelProfile.jpg"
 
 export default function() {
-  return <div>About</div>;
+  return (
+    <div className="content-page-wrapper">
+
+      <div
+      className="left-column"
+      style={{
+        background: "url(" + profilePicture + ") no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+      />
+      <div className="right-column">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt atque deleniti unde nemo quas, inventore officia, aperiam voluptates nam natus labore optio ipsam quidem cum libero soluta enim obcaecati repudiandae.
+Laboriosam esse voluptatem tempore quas neque consequuntur, provident adipisci, distinctio animi nostrum quasi pariatur expedita aliquam eligendi nihil voluptates accusantium autem numquam. Nulla similique voluptas voluptate porro recusandae obcaecati. Reprehenderit.
+Dolor libero sapiente, officiis porro, quo, nesciunt vitae at aliquid eum necessitatibus rerum quod excepturi deleniti tenetur sed fugit unde perspiciatis labore modi adipisci repellat nihil ipsum architecto. Tenetur, atque.
+Eligendi veritatis eos minus culpa nam nemo rerum fuga incidunt commodi illum. Aliquid incidunt, sint, harum blanditiis minus laborum ducimus corrupti omnis, dolores possimus cupiditate nostrum commodi voluptate quas temporibus.
+Quibusdam aspernatur nam animi aliquam officia porro nesciunt minus numquam earum quos ea, alias sunt debitis, odit qui cumque rem quam velit fuga ducimus fugiat ipsam placeat ut! Ducimus, veritatis.
+Eos voluptatem corrupti nulla ullam sunt incidunt sit deleniti architecto pariatur qui officiis ut, laudantium animi numquam dignissimos tenetur earum? Optio aut laborum reprehenderit corporis ipsa perspiciatis rerum, deserunt nisi!
+
+      </div>
+    </div>
+    );
+  
+
 }
